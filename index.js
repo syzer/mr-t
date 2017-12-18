6196426843fa83530c076e6b07306f5d02dbb45e
syzer3@gmail.com <syzer3@gmail.com>
2017-12-18 22:10:34 +0100

    Fixed bug causing the mr-t will not close

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const toTranslateText = process.argv.slice(2).join(' ')
 const querystring = require('querystring')
 const puppeteer = require('puppeteer')
 
-async function translate() {
+async function translate(browser) {
   let page, text
   try {
-    const browser = await puppeteer.launch({
-      headless: true
-    })
-
     page = await browser.newPage()
     await page.goto(`https://translate.google.com/#auto/en/${querystring.escape(toTranslateText)}`)
     await page.waitFor(1000)
     text = await page.$eval(`#result_box`, e => e.innerText)
-
   } catch (err) {
     console.error(err)
   }
@@ -23,6 +18,10 @@ async function translate() {
 }
 
 (async () => {
-  const text = await translate()
+  const browser = await puppeteer.launch({
+    headless: true
+  })
+  const text = await translate(browser)
   console.log(text)
+  browser.close()
 })()
